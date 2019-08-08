2b782363b4266e71fea06a614607da001f946c3e
Anna Spysz <anna.spysz@stackery.io>
2019-08-08 15:32:44 -0700

    Updated deploy script

--- deployHooks/stackery.build.sh
+++ deployHooks/stackery.build.sh
@@ -7,10 +7,12 @@ else
     cp .stackery/template.yaml .aws-sam/build/template.yaml
 fi
 
-( cd src/Function && GOPATH=$PWD && make )
+export GOPATH=$PWD
+
+( cd src/Function && make )
 mkdir -p .aws-sam/build/src/Function
 cp src/Function/main .aws-sam/build/src/Function/main
 
-( cd src/Function2 && GOPATH=$PWD && make )
+( cd src/Function2 && make )
 mkdir -p .aws-sam/build/src/Function2
 cp src/Function2/main .aws-sam/build/src/Function2/main
